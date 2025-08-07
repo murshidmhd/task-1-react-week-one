2df500213d6b4bb2a105a31008bd3614d98460ff
murshidmhd <murshidmuhammad65@gmail.com>
2025-08-07 18:04:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App1.jsx'
-import App from './App.jsx';
+import App from './App1.jsx';
 import nice from './practice.jsx';
 
 createRoot(document.getElementById('root')).render(
